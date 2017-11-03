fbeaef85be0f42802210766349e6320f556f660d
Sachin Chandy <sachinmchandy@gmail.com>
2017-11-03 15:54:13 +0000

    Array slicing, good use of while

---
+++ custom_drop_while.rb
@@ -0,0 +1,26 @@
+
+# def drop_while(array)
+#   result = []
+#   array.each do |ele|
+#     break unless yield ele
+#     result << ele if yield ele
+#   end
+#   array - result 
+# end
+
+# using while
+
+def drop_while(array)
+  index = 0
+  while index < array.size && yield(array[index])
+    index += 1
+  end
+  array[index..-1]
+end
+
+p drop_while([1, 3, 5, 6]) { |value| value.odd? } == [6]
+p drop_while([1, 3, 5, 6]) { |value| value.even? } == [1, 3, 5, 6]
+p drop_while([1, 3, 5, 6]) { |value| true } == []
+p drop_while([1, 3, 5, 6]) { |value| false } == [1, 3, 5, 6]
+p drop_while([1, 3, 5, 6]) { |value| value < 5 } == [5, 6]
+p drop_while([]) { |value| true } == []
