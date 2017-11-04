b6cc91a5f754f3663fe7fa672d0fb2a94026f59d
Sachin Chandy <sachinmchandy@gmail.com>
2017-11-04 11:31:40 +0000

    custom Enumerable#max_by

---
+++ max_by.rb
@@ -0,0 +1,37 @@
+def max_by(array)
+  result = []
+  array.each do |ele|
+    result << yield(ele)
+  end
+  max_index = result.index(result.max)
+  max_index != nil ? array[max_index] : nil
+end
+
+
+# LS solution
+def max_by(array)
+  return nil if array.empty?
+
+  max_element = array.first
+  largest = yield(max_element)
+
+  array[1..-1].each do |item|
+    yielded_value = yield(item)
+    if largest < yielded_value
+      largest = yielded_value
+      max_element = item
+    end
+  end
+  
+  max_element
+end
+
+
+
+
+p max_by([1, 5, 3]) { |value| value + 2 } == 5
+p max_by([1, 5, 3]) { |value| 9 - value } == 1
+p max_by([1, 5, 3]) { |value| (96 - value).chr } == 1
+p max_by([[1, 2], [3, 4, 5], [6]]) { |value| value.size } == [3, 4, 5]
+p max_by([-7]) { |value| value * 3 } == -7
+p max_by([]) { |value| value + 5 } == nil
