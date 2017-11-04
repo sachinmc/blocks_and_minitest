81d9d54742a443d6077d3eb4db47c245ffac426a
Sachin Chandy <sachinmchandy@gmail.com>
2017-11-04 11:28:05 +0000

    custom Array#map

---
+++ custom_map.rb
@@ -0,0 +1,17 @@
+# def map(array)
+#   array.each_with_object([]) do |ele, obj|
+#     obj << yield(ele)
+#   end
+# end
+
+def map(array)
+  result = []
+  array.each { |ele| result << yield(ele) }
+  result
+end
+
+p map([1, 3, 6]) { |value| value**2 } == [1, 9, 36]
+p map([]) { |value| true } == []
+p map(['a', 'b', 'c', 'd']) { |value| false } == [false, false, false, false]
+p map(['a', 'b', 'c', 'd']) { |value| value.upcase } == ['A', 'B', 'C', 'D']
+p map([1, 3, 4]) { |value| (1..value).to_a } == [[1], [1, 2, 3], [1, 2, 3, 4]]
