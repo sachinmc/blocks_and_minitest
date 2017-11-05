bfe79ffdfcd9ec78c680a74fa3f5cdca74220b5c
Sachin Chandy <sachinmchandy@gmail.com>
2017-11-05 08:21:00 +0000

    sandwich code, #split

--- medium/text_analyzer_ls.rb
+++ medium/text_analyzer_ls.rb
@@ -1,3 +1,9 @@
+# also an example of sandwich code.
+# it performs some pre and post processing for some arbitary action
+# here, the pre-processing opens a file and the post-processing closes it.
+# together, they sandwich the action that loads the files contents and
+# passes it to a block.
+
 class TextAnalyzer
   def process
     file = File.open('./text.file', 'r')
